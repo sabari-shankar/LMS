4e8de9f20ab880bfd6c2de02614940775b453155
sabari_shankar <sabarishankar.parthiban@impigertech.com>
2021-12-14 10:22:32 +0530

    change charts

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,7 +30,11 @@ class HomeController extends Controller
         $books = Book::selectRaw('sum(no_of_books) as book_count')->first();
         $data['book_count'] = $books->book_count;
         $data['available_count'] = $data['book_count'] - BookSubscription::all()->count();
-        $data['available_count_percent'] = round(($data['available_count'] / $data['book_count'] ) *100);
+        $availableCountPercent = 0;
+        if($data['book_count'] && $data['available_count']){
+            $availableCountPercent=round(($data['available_count'] / $data['book_count'] ) *100);
+        }
+        $data['available_count_percent'] =$round(($data['available_count'] / $data['book_count'] ) *100);
         if(Auth::user()->is_admin){
             $data['subscription_count'] = BookSubscription::all()->count();
         }else{
